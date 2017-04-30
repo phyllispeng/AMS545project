560f44d5735bd35439795e6fe19d77b56585aefd
Phyllis Peng <Phyllis Peng>
2017-04-30 02:12:42 -0400

    Chans Algo Almost works
    Fix the bug
    TODO ui

--- CGproject/src/cgproject/CGproject.java
+++ CGproject/src/cgproject/CGproject.java
@@ -6,9 +6,6 @@
 package cgproject;
 
 //import java.awt.event.MouseEvent;
-
- 
- 
 import java.io.FileNotFoundException;
 import java.util.*;
 import javafx.scene.input.MouseEvent;
@@ -20,130 +17,175 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
- 
+import javafx.animation.*;
 /**
  *
  * @author Phyllis Peng
  */
 public class CGproject extends Application {
 
- 
     private CHAlgo chalgo;
-        
+
 //        public RadioButton AnimeOn;  
 //        public RadioButton AnimeOff;  
- 
-        
-  
-        @Override
+    public ArrayList<MPoint> mp;
+    public int m;
+    public static Pane c;
+
+    @Override
     public void start(Stage primaryStage) {
-            Insets marginlessInsets= new Insets(5, 5, 5, 5);
-            Button btn = new Button(); 
-            HBox OptionBox = new HBox();
-            Button clearBtn = new Button(); 
-            Button computeBtn = new Button(); 
-            Button loadFile = new Button();
-            BorderPane mainPane = new BorderPane();
-          
-            Pane c = new Pane( );
-            c.resize(800, 600);
-            c.setStyle("-fx-background-color #BDC0BA");
-            OptionBox.setStyle("-fx-background-color:#A8D8B9 ");
-            OptionBox.setAlignment(Pos.BOTTOM_CENTER);
-            OptionBox.setPadding(marginlessInsets);
-            OptionBox.setSpacing(50.0);
-            
-            clearBtn.setText("Clear");
-            computeBtn.setText("Compute");
-            loadFile.setText("Import");
-            
-            c.setOnMouseClicked(new EventHandler<MouseEvent>(){
-                @Override
-                public void handle(MouseEvent e){
-                    MPoint me = respondToClick(e);
-                    Circle circle = new Circle( 5.0);
-                    System.out.println(me);
-                    circle.setFill(Color.BLACK);
-                    //circle.relocate(me.getX(),me.getY());
-                    circle.setCenterX(me.getX());
-                            circle.setCenterY(me.getY());
-                   
-                    c.getChildren().add(circle);
-                    
+        mp = new ArrayList<MPoint>();
+        Insets marginlessInsets = new Insets(5, 5, 5, 5);
+        Button btn = new Button();
+        HBox OptionBox = new HBox();
+        Button clearBtn = new Button();
+        Button computeBtn = new Button();
+        Button loadFile = new Button();
+        BorderPane mainPane = new BorderPane();
+        TextField tf = new TextField();
+        c = new Pane();
+        c.resize(800, 600);
+        c.setStyle("-fx-background-color #BDC0BA");
+        OptionBox.setStyle("-fx-background-color:#A8D8B9 ");
+        OptionBox.setAlignment(Pos.BOTTOM_CENTER);
+        OptionBox.setPadding(marginlessInsets);
+        OptionBox.setSpacing(50.0);
+
+        clearBtn.setText("Clear");
+        computeBtn.setText("Compute");
+        loadFile.setText("Import");
+        tf.setText("m");
+
+        c.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                MPoint me = respondToClick(e);
+                mp.add(me);
+              
+                drawCircle(me);
+            }
+
+        });
+
+        loadFile.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                try {
+                    mp = chalgo.loadCSVFile();
+                    for (int i = 0; i < mp.size(); i++) {
+                        MPoint dot = mp.get(i);
+                        Circle circle = new Circle(5.0);
+                        circle.setFill(Color.BLACK);
+                        double x = (dot.getX() * 25) + 400;
+                        double y = (dot.getY() * 25) + 400;
+                        System.out.println("x is " + x + "y is " + y);
+                        circle.setCenterX(x);
+                        circle.setCenterY(y);
+                        // circle.relocate(x,y);
+                        c.getChildren().add(circle);
+                    }
+                } catch (FileNotFoundException ex) {
+                    System.out.print(ex);
+                }
+            }
+        });
+
+        clearBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                c.getChildren().clear();
+                mp.clear();
+                m=0;
+                System.out.println("Clear");
+            }
+        });
+        computeBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    tf.setStyle("-fx-background-color :white");
+                    m = Integer.parseInt(tf.getText());
+
+                } catch (NumberFormatException ex) { // handle your exception
+                    tf.setStyle("-fx-background-color : #B5495B");
+                    // tf.setStyle("-fx-text-fill :white");
+                }
+                 ArrayList<MPoint> ch = new ArrayList();
+                System.out.println(" m is " + m);
+               
+                // the convex hull arraylist
+                if (m!=0){
+               ch= chalgo.ChanALGO( mp ,  m);
+                System.out.println("chis \n"+ch);
                 }
                 
-            });
-            
-            loadFile.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {  
-                        ArrayList<MPoint> mp = new ArrayList();
-                    try {
-                        mp = chalgo.loadCSVFile();
-                        for ( int i = 0; i < mp.size(); i++){
-                            MPoint dot = mp.get(i);
-                            Circle circle = new Circle( 5.0);
-                            circle.setFill(Color.BLACK);
-                            double x = (dot.getX()*25)+400;
-                            double y = (dot.getY()*25)+400;
-                            System.out.println("x is "+ x + "y is "+y);
-                            circle.setCenterX(x);
-                            circle.setCenterY(y);
-                           // circle.relocate(x,y);
-                            c.getChildren().add(circle);
-                        }
-                    } catch (FileNotFoundException ex) {
-                      System.out.print(ex);
-                    }  
+                // L.add(mp.get(0));
+               
+                for(int k=0; k<ch.size();k++){
+                    drawLines(ch);
+                    
                 }
-            });
-            
-            clearBtn.setOnAction(new EventHandler<ActionEvent>(){
-                @Override
-                public void handle(ActionEvent event){
-                    c.getChildren().clear();
-                    System.out.println("Clear");
-                }
-            });
-            computeBtn.setOnAction(new EventHandler<ActionEvent>(){
-                @Override
-                public void handle(ActionEvent event){
-                    System.out.println("Compute");
-                }
-            });
+            }
+        });
         StackPane root = new StackPane();
-        //root.getChildren().add(btn);
+
         root.getChildren().add(mainPane);
         mainPane.setBottom(OptionBox);
         mainPane.setCenter(c);
-      //  SP.getChildren().add(btn);
- 
-        
+
         Scene scene = new Scene(root, 800, 800);
-        OptionBox.getChildren().addAll(clearBtn,computeBtn,loadFile);
+        OptionBox.getChildren().addAll(tf, computeBtn, loadFile, clearBtn);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-  
-    public static MPoint respondToClick(MouseEvent me){
+
+    public static MPoint respondToClick(MouseEvent me) {
         MPoint mp = new MPoint();
         double x = (double) me.getX();
         double y = (double) me.getY();
         mp.setX(x);
         mp.setY(y);
-        
+
         return mp;
     }
-  
-  
-  
- 
+    public static void drawCircle(MPoint P){
+          Circle circle = new Circle(5.0);
+                
+                circle.setFill(Color.BLACK);
+                //circle.relocate(me.getX(),me.getY());
+                circle.setCenterX(P.getX());
+                circle.setCenterY(P.getY());
+
+                c.getChildren().add(circle);
+    }
+    public static void drawLines(ArrayList<MPoint>  L) {
+        ArrayList<MPoint> newL = new ArrayList(); 
+        newL= L;
+        newL.add(L.get(0));
+        
+        for (int i = 0; i < newL.size() - 1; i++) {
+            Line lineA = new Line();
+            MPoint start = newL.get(i);
+            MPoint end = newL.get(i+1);
+            lineA.setStartX(start.getX());
+            lineA.setStartY(start.getY());
+            lineA.setEndX(end.getX());
+            lineA.setEndY(end.getY());
+       
+            lineA.setStrokeWidth(3);
+            
+            c.getChildren().add(lineA);
+        }
+    }
+
 }
- 
--- CGproject/src/cgproject/CHAlgo.java
+++ CGproject/src/cgproject/CHAlgo.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cgproject;
+
 import java.lang.Math.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -12,14 +12,15 @@ import java.io.FileReader;
 
 //import cgproject.MPoint;
 import java.util.*;
+
 /**
  *
  * @author Phyllis Peng
  */
-
-
 public class CHAlgo {
+
     
+public CGproject cg;
     /**
      *
      * @param L
@@ -27,55 +28,89 @@ public class CHAlgo {
      * @param m
      * @return
      */
-    
-    
-    public static ArrayList ChanALGO(ArrayList<MPoint> L , int m){
-      
-       ArrayList  PList =split(L,m);
-         
+    public static ArrayList<MPoint> ChanALGO(ArrayList<MPoint> L, int m) {
+        ArrayList<MPoint> Lcopy = new ArrayList();
+        Lcopy.addAll(L);
+        
+        ArrayList<ArrayList<MPoint>> PList = split(L, m);
+
         ArrayList Polygon = new ArrayList();
-         Stack st = new Stack();
         //compute convex Hall
-        for (int j =0; j<PList.size();j++){
-             ArrayList<MPoint> FindLow =  FindLowest((ArrayList<MPoint>) PList.get(j));
-             TreeSet<MPoint> L1 = sortAngle(FindLow);
-   
-             ArrayList<MPoint> CG = new ArrayList();
-             CG.addAll(L1);
-                            
-             st = ComputeHull(CG);
-            Polygon.add(st);
+        for (int j = 0; j < PList.size(); j++) {
+            int cellSize = PList.get(j).size();
+            switch (cellSize) {
+                case 1:
+                    Polygon.add(PList.get(j));
+                    break;
+                case 2:
+                    Polygon.add(PList.get(j));
+                    break;
+                default:
+                    ArrayList<MPoint> FindLow = FindLowest((ArrayList<MPoint>) PList.get(j));
+                    TreeSet<MPoint> L1 = sortAngle(FindLow);
+                    ArrayList<MPoint> CG = new ArrayList();
+                    CG.addAll(L1);
+                    //Polygon is an array with all the comvex hulls
+                    // Polygon = [ [hull_1],[hull_2],[hull_3],...]
+                    Polygon.add(ComputeHull(CG));
+                    break;
+            }
+        }
+        
+        //polygon is the graham scan result
+       // initial p0 and p1 
+       //p is the convex hull out put
+       System.out.println("CHull arraylist \n" + Polygon);
+       ArrayList<MPoint> P = new ArrayList();
+  
+       
+       
+        MPoint p1 = FindLowest(Lcopy).get(0);
+        MPoint p0 = new MPoint(p1.getX()-10,0);
+        MPoint pn1 = new MPoint();
+        P.add(0,p0);
+        P.add(1,p1);
+
+        for (int k = 1; k< m;k++){
+            System.out.println("K is "+k+"\n==================================================");
+            for (int i =0; i<PList.size();i++){
+                 
+                // test the largest angle in the sub convex hull
+                 pn1= MAXAngle(P.get(k),P.get(k-1),PList.get(i));
+                 
+            }
              
+           
+           P.add(pn1);
+            
+            if (pn1 == P.get(1)){
+                return P;
+            }
         }
-       //polygon is the grahm scan result
-    return Polygon;
+        return P;
     }
- 
+    public static int largestIndex(ArrayList<Double> A){
+        int index =0;
+        double max = A.get(0);
+        for (int i =0; i<A.size();i++){
+            if(max < A.get(i)){
+                max =A.get(i);
+              index = i;
+              
+            }
+        }
+        System.out.println("\n~~index here is" + index);
+        return index;
+    }
     /**
      *
      * @param a
      * @param b
      * @param c
-     * @return
-     * Method to test out if c is strictly on the left of ab 
+     * @return Method to test out if c is strictly on the left of ab
      */
-    public static boolean Left(MPoint a, MPoint b, MPoint c){
-        if (PointArea(a,b,c)>0){
-            return true;
-        }
-        return false;
-    }
-    
-    /**
-     *
-     * @param a
-     * @param b
-     * @param c
-     * @return
-     * Method to test out if c is on the left or on of ab 
-     */
-    public static boolean OnLeft(MPoint a, MPoint b, MPoint c){
-        if (PointArea(a,b,c)>=0){
+    public static boolean Left(MPoint a, MPoint b, MPoint c) {
+        if (PointArea(a, b, c) > 0) {
             return true;
         }
         return false;
@@ -86,229 +121,227 @@ public class CHAlgo {
      * @param a
      * @param b
      * @param c
+     * @return Method to test out if c is on the left or on of ab
      */
-    public static MPoint LargestAngle(MPoint a, MPoint b, MPoint c){
-        MPoint largest = new MPoint();
-        double DA = PointDistance(b,c);
-        double DB=PointDistance(a,c);
-        double DC=PointDistance(a,b);
-        
-        double LAR = Math.max(DA,Math.max(DB,DC));
-        if (LAR == DA){
-            largest = a;
-        }else if(LAR == DB){
-            largest = b;
-        }else{
-            largest = c;
-        }
-         
-        return largest; 
-    } 
-    /**
-     *
-     * @param a
-     * @param b
-     * @param c
-     * @return
-     * Method to test out if c is collinear with ab 
-     */
-    public static boolean Colinear(MPoint a, MPoint b, MPoint c){
-        if (PointArea(a,b,c)==0){
+    public static boolean OnLeft(MPoint a, MPoint b, MPoint c) {
+        if (PointArea(a, b, c) >= 0) {
             return true;
         }
         return false;
     }
-    
+
+    /**
+     *
+     * @param p1 the center point
+     * @param p2 the pre point
+     * @param Q list of Qs
+     * @return 
+     */
+    public static MPoint MAXAngle(MPoint p1, MPoint p2, ArrayList<MPoint> Q) {
+        ArrayList<Double> Angles = new ArrayList();
+       
+        for (int i =0; i<Q.size();i++){
+            //test with every single point in the point list of q
+        double dis12 =  PointDistance(p1,p2);
+        double dis13 = PointDistance(p1,Q.get(i));
+        double dis23 =  PointDistance(p2,Q.get(i));
+        double result = (dis12*dis12 +dis13*dis13 - dis23*dis23)/(2*dis12*dis13);
+        double angle = Math.acos(result);
+        Angles.add(Math.toDegrees(angle));
+        
+        }
+        System.out.print(Angles);
+        
+//        System.out.println("angles are \n"+ Angles);
+        int index = largestIndex(Angles);
+        
+         System.out.println("the index is  " + index);
+         System.out.println("Q is"+Q);
+        return Q.get(index);
+    }
+
     /**
      *
      * @param a
      * @param b
      * @param c
-     * @return
-     * Compute to the area between a,b,c
+     * @return Method to test out if c is collinear with ab
      */
-    public static double PointArea(MPoint a, MPoint b, MPoint c){
-        double A = (b.getX()-a.getX())*(c.getY()-a.getY())-(c.getX()-a.getX())*(b.getY()-a.getY());
-        return A;
+    public static boolean Colinear(MPoint a, MPoint b, MPoint c) {
+        if (PointArea(a, b, c) == 0) {
+            return true;
+        }
+        return false;
     }
-    
+
     /**
      *
      * @param a
      * @param b
-     * @return
-     * Compute the distance between point a and b
+     * @param c
+     * @return Compute to the area between a,b,c
      */
-    public static double PointDistance(MPoint a, MPoint b){
-        double Xdirection = a.getX()-b.getX();
-        double Ydirection = a.getY()-b.getY();
-        double D = Math.sqrt(Math.pow(Xdirection,2)+Math.pow(Ydirection,2));
+    public static double PointArea(MPoint a, MPoint b, MPoint c) {
+        double A = (b.getX() - a.getX()) * (c.getY() - a.getY()) - (c.getX() - a.getX()) * (b.getY() - a.getY());
+        return A;
+    }
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @return Compute the distance between point a and b
+     */
+    public static double PointDistance(MPoint a, MPoint b) {
+        double Xdirection = a.getX() - b.getX();
+        double Ydirection = a.getY() - b.getY();
+        double D = Math.sqrt(Math.pow(Xdirection, 2) + Math.pow(Ydirection, 2));
         return D;
     }
-    public static ArrayList<MPoint> FindLowest(ArrayList<MPoint> P){
-        
-         
-        int c =0;
+
+    public static ArrayList<MPoint> FindLowest(ArrayList<MPoint> P) {
+
+        int c = 0;
         int n = P.size();
-        for (int i = 1; i<n;i++){
-            if ( (P.get(i).getY() < P.get(c).getY()) || ((P.get(i).getY() == P.get(c).getY() ) && (P.get(i).getX() > P.get(c).getX())) ){
+        for (int i = 1; i < n; i++) {
+            if ((P.get(i).getY() < P.get(c).getY()) || ((P.get(i).getY() == P.get(c).getY()) && (P.get(i).getX() > P.get(c).getX()))) {
                 c = i;
             }
         }
-        MPoint temp = P.get(0);
-        
-Collections.swap(P, 0,c);
+      
+        Collections.swap(P, 0, c);
         return P;
     }
-    
-    public static ArrayList<MPoint> loadCSVFile() throws FileNotFoundException{
+
+    public static ArrayList<MPoint> loadCSVFile() throws FileNotFoundException {
         ArrayList<MPoint> MP = new ArrayList();
-        
+
         String csvFilePath = "src/cgproject/";
-         
-        String fileName =csvFilePath+"points.csv";
-        Scanner fromFile = new Scanner(new BufferedReader(new FileReader(fileName))); 
-        while(fromFile.hasNextLine()){
-            String Input =  fromFile.nextLine();
+
+        String fileName = csvFilePath + "points.csv";
+        Scanner fromFile = new Scanner(new BufferedReader(new FileReader(fileName)));
+        while (fromFile.hasNextLine()) {
+            String Input = fromFile.nextLine();
             String[] p = Input.split(",");
             double Px = Double.parseDouble(p[0]);
             double Py = Double.parseDouble(p[1]);
-            
+
             MPoint P = new MPoint();
             P.setX(Px);
             P.setY(Py);
             MP.add(P);
         }
-        
+
         return MP;
     }
-    public static double MAngle (MPoint p0, MPoint pi ){
+
+    public static double MAngle(MPoint p0, MPoint pi) {
         double X = pi.getX() - p0.getX();
         double Y = pi.getY() - p0.getY();
-        
-        double MA = Math.atan2(Y,X);
+
+        double MA = Math.atan2(Y, X);
         return MA;
     }
 
-    
-    public static TreeSet<MPoint> sortAngle(ArrayList<MPoint>  L){
-        MPoint low =  L.get(0);
-    TreeSet<MPoint> SortedSet = new TreeSet<MPoint>(new Comparator<MPoint>(){
+    public static TreeSet<MPoint> sortAngle(ArrayList<MPoint> L) {
+        MPoint low = L.get(0);
+        TreeSet<MPoint> SortedSet = new TreeSet<MPoint>(new Comparator<MPoint>() {
             @Override
             public int compare(MPoint o1, MPoint o2) {
-                if (o1 == o2 || o1.equals(o2)){
-                    
+                if (o1 == o2 || o1.equals(o2)) {
+
                     return 0;
                 }
-                double theta1 = MAngle(low,o1);
-                double theta2 = MAngle(low,o2);
-              
-                if (theta1 < theta2){
+                double theta1 = MAngle(low, o1);
+                double theta2 = MAngle(low, o2);
+
+                if (theta1 < theta2) {
                     return -1;
-                }else if (theta1 > theta2){
+                } else if (theta1 > theta2) {
                     return 1;
-                }
-                else{
-                    double d1 = PointDistance(low,o1);
-                    double d2 = PointDistance(low,o2);
-                    
-                    if (d1<d2){
-                        
+                } else {
+                    double d1 = PointDistance(low, o1);
+                    double d2 = PointDistance(low, o2);
+
+                    if (d1 < d2) {
+
                         return -1;
-                    }else{
-                        
+                    } else {
+
                         return 1;
-                        }
+                    }
                 }
             }
-            
-        }); 
+
+        });
         SortedSet.addAll(L);
         return SortedSet;
     }
-     public static  Stack <MPoint> ComputeHull(ArrayList<MPoint> L ){
-     Stack st =  new Stack();
+
+    public static ArrayList<MPoint> ComputeHull(ArrayList<MPoint> L) {
+        Stack<MPoint> st = new Stack();
+        ArrayList Pgons = new ArrayList();
         MPoint sP = L.get(0); //lowest point is the starting point
         st.push(sP);
         st.push(L.get(1));
         // bottom two whill never be removed
-        int i =2;
-        while (i<L.size() ){
+        int i = 2;
+        while (i < L.size()) {
             MPoint temp = (MPoint) st.pop();
             MPoint p0 = (MPoint) st.peek();
             st.push(temp);
             MPoint p1 = (MPoint) st.peek();
-            
-            if (Left(p0,p1,L.get(i))){
+
+            if (Left(p0, p1, L.get(i))) {
                 st.push(L.get(i));
                 i++;
-            }else{
+            } else {
                 st.pop();
             }
-            
-        }
-        
-        
-        return st;
-    }
-    
-  
 
-    private static ArrayList<MPoint> split(ArrayList<MPoint> L, double m) {
-        double n = L.size();
-         
-        double r = Math.ceil(n/m);
+        }
+
+        while (!st.isEmpty()) {
+            Pgons.add(0, st.pop());
+        }
        
+        return Pgons;
+    }
+
+    private static ArrayList<ArrayList<MPoint>> split(ArrayList<MPoint> L, double m) {
+        double n = L.size();
+
+        double r = Math.ceil(n / m);
+
         ArrayList PList = new ArrayList();
-         
-        int i =0;
-        while (i<r){
-            
-            if (L.size() <m){
+
+        int i = 0;
+        while (i < r) {
+
+            if (L.size() < m) {
                 PList.add(L);
-            }else{
-                 ArrayList<MPoint> temp = new ArrayList();
-                
-                for (int j =0 ;j<m;j++){
-                   
+            } else {
+                ArrayList<MPoint> temp = new ArrayList();
+
+                for (int j = 0; j < m; j++) {
+
                     temp.add(L.get(0));
                     L.remove(0);
                 }
                 PList.add(temp);
-               System.out.println(L);
+                System.out.println(L);
             }
-            
-          
-        i++;
+
+            i++;
         }
-          System.out.println("P is \n"+PList);
+      
         return PList;
-        }
-    
-    
-        public static void main(String[] args) throws FileNotFoundException {
-    
-     
-    
-    MPoint a = new MPoint(1,1);
-    MPoint b = new MPoint(2,1);
-    MPoint c = new MPoint(4,2);
-     
- 
-    
-    ArrayList<MPoint> mpoints = loadCSVFile();
-    
- 
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+       ArrayList<MPoint> aaa = loadCSVFile();
+       System.out.println(ChanALGO(aaa, 3));
   
-//    Stack aa = new Stack();
-//    aa = GS(CG);
-//    System.out.println("stack is"+aa);
-     List d =ChanALGO(mpoints ,  3);
-       System.out.println("=============================================");
-     System.out.println( "d is\n"+d);
-       System.out.println("=============================================");
-     
-     
-     System.out.println(LargestAngle( a,  b,  c));
     }
 }
